60b5ae971f303256c48dfad700491b7afdc057f6
Veselov Artem <aveselov130@gmail.com>
2025-09-20 10:59:15 +0300

    Completed x3

---
+++ build/CMakeFiles/lab_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_1.dir/lab_1.cpp.o"
+  "CMakeFiles/lab_1.dir/lab_1.cpp.o.d"
+  "lab_1"
+  "lab_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
