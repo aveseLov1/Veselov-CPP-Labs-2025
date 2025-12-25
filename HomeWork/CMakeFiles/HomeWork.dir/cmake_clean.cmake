3ea00660fecef2bc31ed0427cdea28069ba91461
Veselov Artem <aveselov130@gmail.com>
2025-12-25 11:39:35 +0300

    almost 3 done

---
+++ HomeWork/CMakeFiles/HomeWork.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork.dir/HomeWork.cpp.o"
+  "CMakeFiles/HomeWork.dir/HomeWork.cpp.o.d"
+  "HomeWork"
+  "HomeWork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
