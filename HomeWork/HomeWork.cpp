bc44b3dfca27066b595f6c3488c56cad495d601f
Veselov Artem <aveselov130@gmail.com>
2025-12-27 15:46:39 +0300

    HomeWork almost done

--- HomeWork/HomeWork.cpp
+++ HomeWork/HomeWork.cpp
@@ -12,11 +12,9 @@ int main() {
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    char words[1000][100];
-    int lengths[1000];
-    int word_count = 0;
-    int pos = 0;
-    bool in_word = false;
+    // Буфер для всего ввода (до точки)
+    char input_buffer[10000];
+    int input_pos = 0;
 
     // Выводим приглашение
     const char* prompt = "Введите текст: ";
@@ -24,6 +22,7 @@ int main() {
         std::cout.put(prompt[i]);
     }
 
+    // Фаза 1: ТОЛЬКО ввод, сохранение всего в буфер
     while (true) {
         char c = std::cin.get();
 
@@ -31,70 +30,41 @@ int main() {
         if (c == '.') {
             std::cout.put('.');
             std::cout.put('\n');
-            if (in_word) {
-                lengths[word_count - 1] = pos;
+            // Добавляем точку в буфер как маркер конца
+            if (input_pos < 9999) {
+                input_buffer[input_pos] = '.';
+                ++input_pos;
             }
             break;
         }
 
         // Backspace
         if (c == 127 || c == 8) {
-            // Всегда пытаемся стереть символ на экране
-            std::cout.put('\b');
-            std::cout.put(' ');
-            std::cout.put('\b');
-
-            // Обновляем внутренние структуры данных
-            if (in_word && pos > 0) {
-                // Удаляем букву из текущего слова
-                --pos;
-
-                // Если слово стало пустым
-                if (pos == 0) {
-                    in_word = false;
-                    // Уменьшаем счетчик только если слово было не пустое изначально
-                    if (word_count > 0) {
-                        --word_count;
-                    }
-                }
+            if (input_pos > 0) {
+                --input_pos;
+                std::cout.put('\b');
+                std::cout.put(' ');
+                std::cout.put('\b');
             }
-            // Если мы между словами (после пробела)
-            else if (!in_word && word_count > 0) {
-                // Возвращаемся к предыдущему слову
-                in_word = true;
-                pos = lengths[word_count - 1];
-                // Здесь НЕ уменьшаем word_count, так как слово уже существует
-            }
-            // Если мы в начале и нет слов, просто игнорируем
-            // (уже стерли символ на экране выше)
             continue;
         }
 
-        // Английские буквы
+        // Английские буквы - сохраняем в буфер
         if (c >= 'a' && c <= 'z') {
             std::cout.put(c);
-
-            if (!in_word) {
-                in_word = true;
-                ++word_count;
-                pos = 0;
-            }
-
-            // Сохраняем букву
-            if (pos < 100) {
-                words[word_count - 1][pos] = c;
-                ++pos;
+            if (input_pos < 9999) {
+                input_buffer[input_pos] = c;
+                ++input_pos;
             }
             continue;
         }
 
-        // Пробел
+        // Пробел - сохраняем в буфер
         if (c == ' ') {
             std::cout.put(' ');
-
-            if (in_word) {
-                lengths[word_count - 1] = pos;
-                in_word = false;
+            if (input_pos < 9999) {
+                input_buffer[input_pos] = ' ';
+                ++input_pos;
             }
             continue;
         }
@@ -104,6 +74,44 @@ int main() {
 
     // Восстанавливаем настройки терминала
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    // Фаза 2: Парсинг буфера на слова (уже после ввода точки)
+    char words[1000][100];
+    int lengths[1000];
+    int word_count = 0;
+    int pos_in_word = 0;
+    bool in_word = false;
+
+    for (int i = 0; i < input_pos; ++i) {
+        char c = input_buffer[i];
+
+        if (c == '.') {
+            // Точка - конец текста
+            if (in_word) {
+                lengths[word_count - 1] = pos_in_word;
+            }
+            break;
+        }
+
+        if (c >= 'a' && c <= 'z') {
+            if (!in_word) {
+                in_word = true;
+                ++word_count;
+                pos_in_word = 0;
+            }
+
+            if (pos_in_word < 99) {
+                words[word_count - 1][pos_in_word] = c;
+                ++pos_in_word;
+            }
+        }
+        else if (c == ' ') {
+            if (in_word) {
+                lengths[word_count - 1] = pos_in_word;
+                in_word = false;
+            }
+        }
+    }
 
     if (word_count == 0) {
         const char* msg = "Нет слов для обработки\n";
@@ -120,7 +128,7 @@ int main() {
         last_word[i] = words[word_count - 1][i];
     }
 
-    // Вывод результата
+    // Фаза 3: Обработка и вывод результата
     bool first_output = true;
     bool has_output = false;
 
@@ -175,3 +183,184 @@ int main() {
     std::cout.put('\n');
     return 0;
 }
+
+
+
+
+// #include <iostream>
+// #include <termios.h>
+// #include <unistd.h>
+
+// int main() {
+//     // Сохраняем текущие настройки терминала
+//     struct termios oldt, newt;
+//     tcgetattr(STDIN_FILENO, &oldt);
+//     newt = oldt;
+
+//     // Отключаем канонический режим и эхо
+//     newt.c_lflag &= ~(ICANON | ECHO);
+//     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+//     char words[1000][100];
+//     int lengths[1000];
+//     int word_count = 0;
+//     int pos = 0;
+//     bool in_word = false;
+
+//     // Выводим приглашение
+//     const char* prompt = "Введите текст: ";
+//     for (int i = 0; prompt[i] != '\0'; ++i) {
+//         std::cout.put(prompt[i]);
+//     }
+
+//     while (true) {
+//         char c = std::cin.get();
+
+//         // Если точка - завершаем ввод
+//         if (c == '.') {
+//             std::cout.put('.');
+//             std::cout.put('\n');
+//             if (in_word) {
+//                 lengths[word_count - 1] = pos;
+//             }
+//             break;
+//         }
+
+//         // Backspace
+//         if (c == 127) {
+//             // Всегда пытаемся стереть символ на экране
+//             std::cout.put('\b');
+//             std::cout.put(' ');
+//             std::cout.put('\b');
+
+//             // Обновляем внутренние структуры данных
+//             if (in_word && pos > 0) {
+//                 // Удаляем букву из текущего слова
+//                 --pos;
+
+//                 // Если слово стало пустым
+//                 if (pos == 0) {
+//                     in_word = false;
+//                     // Уменьшаем счетчик только если слово было не пустое изначально
+//                     if (word_count > 0) {
+//                         --word_count;
+//                     }
+//                 }
+//             }
+//             // Если мы между словами (после пробела)
+//             else if (!in_word && word_count > 0) {
+//                 // Возвращаемся к предыдущему слову
+//                 in_word = true;
+//                 pos = lengths[word_count - 1];
+//                 // Здесь НЕ уменьшаем word_count, так как слово уже существует
+//             }
+//             // Если мы в начале и нет слов, просто игнорируем
+//             // (уже стерли символ на экране выше)
+//             continue;
+//         }
+
+//         // Английские буквы
+//         if (c >= 'a' && c <= 'z') {
+//             std::cout.put(c);
+
+//             if (!in_word) {
+//                 in_word = true;
+//                 ++word_count;
+//                 pos = 0;
+//             }
+
+//             // Сохраняем букву
+//             if (pos < 100) {
+//                 words[word_count - 1][pos] = c;
+//                 ++pos;
+//             }
+//             continue;
+//         }
+
+//         // Пробел
+//         if (c == ' ') {
+//             std::cout.put(' ');
+
+//             if (in_word) {
+//                 lengths[word_count - 1] = pos;
+//                 in_word = false;
+//             }
+//             continue;
+//         }
+
+//         // Остальные символы игнорируем
+//     }
+
+//     // Восстанавливаем настройки терминала
+//     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+//     if (word_count == 0) {
+//         const char* msg = "Нет слов для обработки\n";
+//         for (int i = 0; msg[i] != '\0'; ++i) {
+//             std::cout.put(msg[i]);
+//         }
+//         return 0;
+//     }
+
+//     // Сохраняем последнее слово
+//     char last_word[100];
+//     int last_len = lengths[word_count - 1];
+//     for (int i = 0; i < last_len; ++i) {
+//         last_word[i] = words[word_count - 1][i];
+//     }
+
+//     // Вывод результата
+//     bool first_output = true;
+//     bool has_output = false;
+
+//     for (int i = 0; i < word_count - 1; ++i) {
+//         // Проверяем, отличается ли от последнего
+//         bool different = false;
+
+//         if (lengths[i] != last_len) {
+//             different = true;
+//         } else {
+//             for (int j = 0; j < lengths[i]; ++j) {
+//                 if (words[i][j] != last_word[j]) {
+//                     different = true;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         if (different) {
+//             // Проверяем условие: первая буква встречается ровно 2 раза
+//             char first_letter = words[i][0];
+//             int occurrences = 0;
+
+//             for (int j = 0; j < lengths[i]; ++j) {
+//                 if (words[i][j] == first_letter) {
+//                     ++occurrences;
+//                 }
+//             }
+
+//             if (occurrences == 2) {
+//                 if (!first_output) {
+//                     std::cout.put(' ');
+//                 }
+
+//                 for (int j = 0; j < lengths[i]; ++j) {
+//                     std::cout.put(words[i][j]);
+//                 }
+
+//                 first_output = false;
+//                 has_output = true;
+//             }
+//         }
+//     }
+
+//     if (!has_output) {
+//         const char* msg = "Нет подходящих слов";
+//         for (int i = 0; msg[i] != '\0'; ++i) {
+//             std::cout.put(msg[i]);
+//         }
+//     }
+
+//     std::cout.put('\n');
+//     return 0;
+// }
