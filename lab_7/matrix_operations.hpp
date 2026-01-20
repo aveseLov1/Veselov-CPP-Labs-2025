d7850a91ea04344fd64a6659c18a9fa810e24e7e
Veselov Artem <aveselov130@gmail.com>
2026-01-20 19:46:41 +0300

    lab-7 mayde will with clag-format?

--- lab_7/matrix_operations.hpp
+++ lab_7/matrix_operations.hpp
@@ -6,27 +6,25 @@
 namespace MatrixModule {
 
 // Функция выделения памяти для двухмерного динамического массива
-double **allocateMatrix(std::size_t numberOfRows, std::size_t numberOfColumns);
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays, hicpp-avoid-c-arrays, modernize-avoid-c-arrays)
+double** allocateMatrix(std::size_t numberOfRows, std::size_t numberOfColumns);
 
 // Функция освобождения памяти
-void freeMatrix(double **matrix, std::size_t numberOfRows);
+void freeMatrix(double** matrix, std::size_t numberOfRows);
 
 // Функция вычисления факториала
 long long factorial(int n);
 
 // Функция заполнения матрицы по заданным формулам
-void fillMatrix(double **matrix, std::size_t numberOfRows,
-                std::size_t numberOfColumns, double x = 1.0);
+void fillMatrix(double** matrix, std::size_t numberOfRows, std::size_t numberOfColumns, double x = 1.0);
 
 // Функция вывода динамической матрицы
-void printDynamicMatrix(const double *const *matrix, std::size_t numberOfRows,
-                        std::size_t numberOfColumns, int precision = 6,
+void printDynamicMatrix(const double* const* matrix, std::size_t numberOfRows, std::size_t numberOfColumns, int precision = 6,
                         bool scientificFormat = false, int maxLineLength = 80);
 
 // Функция вывода статической матрицы через вспомогательный массив указателей
-void printStaticMatrix(const double *const rows[], std::size_t numberOfRows,
-                       std::size_t numberOfColumns, int precision = 6,
+void printStaticMatrix(const double* const rows[], std::size_t numberOfRows, std::size_t numberOfColumns, int precision = 6,
                        bool scientificFormat = false, int maxLineLength = 80);
-} // namespace MatrixModule
+}  // namespace MatrixModule
 
-#endif // MATRIX_OPERATIONS_HPP
+#endif  // MATRIX_OPERATIONS_HPP
